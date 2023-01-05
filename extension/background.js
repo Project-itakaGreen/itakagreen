332646cd17d11064d68f1c47c40468f84a352818
adrien <a.charrier@web-adn.com>
2023-01-05 15:51:52 +0100

    feat - get body data size

--- extension/background.js
+++ extension/background.js
@@ -6,18 +6,32 @@ chrome.runtime.onInstalled.addListener(function () {
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   url = tab.url;
-  console.log('url de base: ' + url);
 
   var url = url;
   var regex = /^(https?:\/\/)?([\w.-]+\.[a-z]{2,6})/;
   var match = url.match(regex);
 
   if (match) {
-
     urlForApi = match[0]
     console.log('le bon Url: ' + urlForApi)
   }
 
 });
 
+
+chrome.webRequest.onCompleted.addListener(
+  function(details) {
+    let responseSize = 0;
+
+    if (details.responseHeaders) {
+      const contentLengthHeader = details.responseHeaders.find(header => header.name.toLowerCase() == "content-length");
+      if (contentLengthHeader) {
+        responseSize = parseInt(contentLengthHeader.value);
+        console.log(responseSize)
+      }
+    }
+  },
+  {urls: ['<all_urls>']},
+  ["responseHeaders"]
+);
 
