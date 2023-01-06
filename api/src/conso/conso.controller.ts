c88500b00f4b0ea92568b0f1a290b245a15a98a4
Johan <j.crochemore@it-students.fr>
2023-01-06 14:12:34 +0100

    feat - improvement of last commit

--- api/src/conso/conso.controller.ts
+++ api/src/conso/conso.controller.ts
@@ -4,24 +4,30 @@ import { ConsoService } from './conso.service';
 
 import { Param } from '@nestjs/common';
 
+const DEFAULT_DAYS_PERIOD = 7;
+
+const DEFAULT_TOTAL_PERIOD = 1;
+
+const DEFAULT_DOMAIN_PERIOD  = 1;
+
 @Controller('conso')
 export class ConsoController {
 
     constructor(private readonly consoService: ConsoService) {}
 
-    @Get('total/:number?')
-    getTotal(@Param('number') number: number) {
-        return this.consoService.getTotal(number??1);
+    @Get('total/:period?')
+    getTotal(@Param('period') period: number) {
+        return this.consoService.getTotal( period ?? DEFAULT_TOTAL_PERIOD );
       }
 
-    @Get('day/:number?')
-    getDay(@Param('number') number: number) {
-        return this.consoService.getDay(number??7);
+    @Get('day/:period?')
+    getDay(@Param('period') period: number) {
+        return this.consoService.getDay( period ?? DEFAULT_DAYS_PERIOD );
       }
 
-    @Get('domain/:number?')
-    getDomain(@Param('number') number: number) {
-        return this.consoService.getDomain(number??1);
+    @Get('domain/:period?')
+    getDomain(@Param('period') period: number) {
+        return this.consoService.getDomain( period ?? DEFAULT_DOMAIN_PERIOD );
       }
 
 }
