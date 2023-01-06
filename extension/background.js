e84eaffa588087bc62fe860ad20abae4546fb834
Malek Medjoudj <m.medjoudj@it-students.fr>
2023-01-06 11:20:35 +0100

    comment

--- extension/background.js
+++ extension/background.js
@@ -1,10 +1,15 @@
+// init the data of api for the active tab  
 let activeSiteData = null;
+// init the last origin url
 let lastOriginUrl = "";
 
+
+// check if the extension is installed or updated
 chrome.runtime.onInstalled.addListener(function () {
   console.log("Extension has been installed");
 });
 
+// get the origin of the url
 function getOrigineFromUrl(url) {
   if (url.indexOf("chrome://") != 0 && url.length != 0) {
     const parsedUrl = new URL(url);
@@ -12,22 +17,24 @@ function getOrigineFromUrl(url) {
   }
 }
 
+//verify if the url is the same as the last one
 function getDomainData(originUrl) {
   if (originUrl != lastOriginUrl && originUrl != undefined) {
     lastOriginUrl = originUrl;
     // TODO: call api
     console.log("call api with :" + originUrl);
-    return {};//objet reçu de l'api
+    return {};//object with the data of api
   }
 }
 
+// get the url of the active tab
 chrome.tabs.onActivated.addListener(function (activeInfo) {
   chrome.tabs.get(activeInfo.tabId, function (tab) {
     const activeSiteData = getDomainData(getOrigineFromUrl(tab.url));
   });
  });
 
-
+//  get the url of loaded tab 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   const activeSiteData = getDomainData(getOrigineFromUrl(tab.url));
 });
