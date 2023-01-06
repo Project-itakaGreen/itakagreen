a87fe334bfa9a593bc44be396ec6cb31dd796cbc
Malek Medjoudj <m.medjoudj@it-students.fr>
2023-01-06 10:03:06 +0100

    get domaine url on page load and chage tabs

--- extension/background.js
+++ extension/background.js
@@ -1,37 +1,53 @@
+let activeSiteData = null;
+let lastOriginUrl = "";
+
 chrome.runtime.onInstalled.addListener(function () {
-  console.log("its ok ");
-
-
+  console.log("Extension has been installed");
 });
+
+function getOrigineFromUrl(url) {
+  if (url.indexOf("chrome://") != 0 && url.length != 0) {
+    const parsedUrl = new URL(url);
+    return parsedUrl.origin ;
+  }
+}
+
+function getDomainData(originUrl) {
+  if (originUrl != lastOriginUrl && originUrl != undefined) {
+    lastOriginUrl = originUrl;
+    // TODO: call api
+    console.log("call api with :" + originUrl);
+    return {};//objet reçu de l'api
+  }
+}
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+  chrome.tabs.get(activeInfo.tabId, function (tab) {
+    const activeSiteData = getDomainData(getOrigineFromUrl(tab.url));
+  });
+ });
+
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  url = tab.url;
-
-  var url = url;
-  var regex = /^(https?:\/\/)?([\w.-]+\.[a-z]{2,6})/;
-  var match = url.match(regex);
-
-  if (match) {
-    urlForApi = match[0]
-    console.log('le bon Url: ' + urlForApi)
-  }
-
+  const activeSiteData = getDomainData(getOrigineFromUrl(tab.url));
 });
 
 
-chrome.webRequest.onCompleted.addListener(
-  function(details) {
-    let responseSize = 0;
+// chrome.webRequest.onCompleted.addListener(
+//   function(details) {
+//     let responseSize = 0;
 
-    if (details.responseHeaders) {
-      const contentLengthHeader = details.responseHeaders.find(header => header.name.toLowerCase() == "content-length");
-      if (contentLengthHeader) {
-        responseSize = parseInt(contentLengthHeader.value);
-        console.log(responseSize)
-      }
-    }
-  },
-  {urls: ['<all_urls>']},
-  ["responseHeaders"]
-);
+//     if (details.responseHeaders) {
+//       const contentLengthHeader = details.responseHeaders.find(header => header.name.toLowerCase() == "content-length");
+//       if (contentLengthHeader) {
+//         responseSize = parseInt(contentLengthHeader.value);
+//         // console.log(responseSize)
+//       }
+//     }
+//   },
+//   {urls: ['<all_urls>']},
+//   ["responseHeaders"]
+// );
+
+
 
