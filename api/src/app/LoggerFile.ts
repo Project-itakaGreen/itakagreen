37314ce49cf7758bfecfeb07b30458b69c0ba7fc
Johan <j.crochemore@it-students.fr>
2023-01-06 14:42:28 +0100

    feat - LoggerFile class moves from main

---
+++ api/src/app/LoggerFile.ts
@@ -0,0 +1,97 @@
+
+import { LoggerService } from '@nestjs/common';
+
+export class LoggerFile implements LoggerService
+{
+  /**
+    * Write a 'log' level log.
+    */
+
+  log(message: any, ...optionalParams: any[]) {
+
+    this.fsToFile('LOG',message,optionalParams);
+
+  }
+
+  /**
+    * Write an 'error' level log.
+    */
+
+  error(message: any, ...optionalParams: any[]) {
+
+    this.fsToFile('ERROR',message,optionalParams);
+
+  }
+
+  /**
+    * Write a 'warn' level log.
+    */
+
+  warn(message: any, ...optionalParams: any[]) {
+
+    this.fsToFile('WARN',message,optionalParams);
+
+  }
+
+  /**
+    * Write a 'debug' level log.
+    */
+
+  debug?(message: any, ...optionalParams: any[]) {
+
+    this.fsToFile('DEBUG',message,optionalParams);
+
+  }
+
+  /**
+    * Write a 'verbose' level log.
+    */
+
+  verbose?(message: any, ...optionalParams: any[]) {
+
+    this.fsToFile('VERBOSE',message,optionalParams);
+
+  }
+
+
+  /*
+  * Fonction qui prend en paramètre le type de log et les paramètres reçu des fonctions
+  * du LoggerService afin d'écrire dans le message dans un fichier texte
+  */
+
+  fsToFile(type: any, message: any, ...optionalParams: any[])
+  {
+    const fs = require('fs');
+
+    // recuperation de la date
+
+    let date = new Date();
+
+    let day = ('0'+date.getDate()).slice(-2);
+
+    let month = ('0'+date.getMonth()+1).slice(-2);
+
+    let year = date.getFullYear();
+
+    let hours = ('0'+date.getHours()).slice(-2);
+
+    let minutes = ('0'+date.getMinutes()).slice(-2);
+
+    let seconds = ('0'+date.getSeconds()).slice(-2);
+
+    // creation du dossier de logs si il n'existe pas
+    let dir = './logs';
+
+    if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir);
+    }
+
+    // assignation du nom du fichier txt de log
+    let myFile = `logs/${day}-${month}-${year}-log.txt`;
+
+    // ecriture du log dans le fichier txt
+    let log = fs.createWriteStream(myFile,{ flags: 'a'});
+    log.write(`[${type}] ${day}-${month}-${year} ${hours}:${minutes}:${seconds}  [${optionalParams}]  ${message}\n`);
+    log.end();
+  }
+}
