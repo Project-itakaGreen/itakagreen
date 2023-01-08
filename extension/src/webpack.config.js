f709ce909de4bf066248af7815252bf922cb359f
adrien <a.charrier@web-adn.com>
2023-01-08 16:09:31 +0100

    FIX - copy all files into the dist

--- extension/src/webpack.config.js
+++ extension/src/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -7,5 +8,26 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, '../dist')
-  }
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{
+          from: './images',
+          to: '../dist/images',
+        },
+        {
+          from: './manifest.json',
+          to: '../dist/manifest.json',
+        },
+        {
+          from: './popup.html',
+          to: '../dist/popup.html',
+        },
+        {
+          from: './popup.css',
+          to: '../dist/popup.css',
+        }
+      ],
+    })
+  ]
 };
