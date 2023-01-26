3ad46f2a8a473091f534e42be9c6ce21b98e28fa
JohanCrochemore <crochemore.johan@gmail.com>
2023-01-26 17:25:46 +0100

    chart userdetails with domain details

--- front/pages/details.tsx
+++ front/pages/details.tsx
@@ -2,23 +2,58 @@ import CardGraph from "../components/shared/CardGraph";
 
 export default function Details() {
   const data = [
-    { date: "19/01/2023", co2: 15 },
-    { date: "20/01/2023", co2: 35 },
-    { date: "21/01/2023", co2: 10 },
-    { date: "23/01/2023", co2: 89 },
-    { date: "24/01/2023", co2: 6 },
-    { date: "25/01/2023", co2: 173 },
+    {
+      day: "20/01/2023",
+      domains: [{ name: "twitter", co2: 35 }],
+      totalCo2: 35,
+    },
+    {
+      day: "21/01/2023",
+      domains: [{ name: "twitter", co2: 10 }],
+      totalCo2: 10,
+    },
+    {
+      day: "23/01/2023",
+      domains: [
+        { name: "youtube", co2: 54 },
+        { name: "chatgpt", co2: 35 },
+      ],
+      totalCo2: 89,
+    },
+    { day: "24/01/2023", domains: [{ name: "youtube", co2: 6 }], totalCo2: 6 },
+    {
+      day: "25/01/2023",
+      domains: [
+        { name: "youtube", co2: 60 },
+        { name: "twitter", co2: 45 },
+        { name: "chatgpt", co2: 35 },
+        { name: "google", co2: 33 },
+      ],
+      totalCo2: 173,
+    },
   ];
-  const chartLabel = data.map((e) => e.date);
-  const chartData = data.map((e) => e.co2);
-  const arrayType = ['bar','doughnut','pie','line','polarArea','radar'];
-  const type=arrayType[Math.floor(Math.random()*arrayType.length)]; // bar doughnut pie line polarArea radar
+  const chartLabel = data.map((e) => e.day);
+  const chartData = data.map((e) => e.totalCo2);
+  const allDaysdomainsArray=data.map(e=>{
+        let domainString="\n";
+        e.domains.map(domain=>{
+            domainString += domain.name+" : "+domain.co2+" grammes \n";
+        })
+        return domainString
+    });
+
+  const arrayType = ["bar", "doughnut", "pie", "line", "polarArea", "radar"];
+  const type = arrayType[Math.floor(Math.random() * arrayType.length)]; // bar doughnut pie line polarArea radar
+  const afterBody = (context:any)=>
+  {
+    return allDaysdomainsArray[context[0].dataIndex];
+  }
 
   return (
     <>
       <h1>User Details</h1>
 
-      <CardGraph chartLabel={chartLabel} chartData={chartData} type={type}/>
+      <CardGraph chartLabel={chartLabel} chartData={chartData} type={type} afterBody={afterBody}/>
     </>
   );
 }
