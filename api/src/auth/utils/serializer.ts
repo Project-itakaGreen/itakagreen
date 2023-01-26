c6b6c8f6047a53a6bc794b7805fbf50715caad40
adrien <a.charrier@web-adn.com>
2023-01-26 13:11:53 +0100

    fix typescript function

--- api/src/auth/utils/serializer.ts
+++ api/src/auth/utils/serializer.ts
@@ -1,7 +1,8 @@
-import { PassportSerializer } from '@nestjs/passport';
-import { AuthService } from '../auth.service';
 import { Inject, Injectable } from '@nestjs/common';
+import { PassportSerializer } from '@nestjs/passport';
 import { User } from 'src/user/entities/user.entity';
+
+import { AuthService } from '../auth.service';
 
 // check if user is valide for each endpoint
 @Injectable()
@@ -12,13 +13,17 @@ export class SessionSerializer extends PassportSerializer {
     super();
   }
 
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  serializeUser(user: User, done: Function) {
+  serializeUser(
+    user: User,
+    done: (err: Error | null, user: User | null) => null | User,
+  ) {
     done(null, user);
   }
 
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  async deserializeUser(payload: any, done: Function) {
+  async deserializeUser(
+    payload: any,
+    done: (err: Error | null, user: User | null) => null | User,
+  ) {
     const user = await this.authService.findUserById(payload.id);
     return user ? done(null, user) : done(null, null);
   }
