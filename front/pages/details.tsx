a3fac3dd51ebcaa417efafa80580eb57c883cc52
JohanCrochemore <crochemore.johan@gmail.com>
2023-01-30 11:51:16 +0100

    add detail to user chart

--- front/pages/details.tsx
+++ front/pages/details.tsx
@@ -1,6 +1,9 @@
 import CardGraph from "../components/shared/CardGraph";
 
 export default function Details() {
+
+    const KEEP_VALUES = 3;
+
   const data = [
     {
       day: "20/01/2023",
@@ -28,25 +31,41 @@ export default function Details() {
         { name: "twitter", co2: 45 },
         { name: "chatgpt", co2: 35 },
         { name: "google", co2: 33 },
+        { name: "mozilla", co2: 63 },
+        { name: "twitter", co2: 13 },
       ],
       totalCo2: 173,
     },
   ];
   const chartLabel = data.map((e) => e.day);
   const chartData = data.map((e) => e.totalCo2);
+
   const allDaysdomainsArray=data.map(e=>{
-        let domainString="\n";
-        e.domains.map(domain=>{
-            domainString += domain.name+" : "+domain.co2+" grammes \n";
+        let domainString = new Array("\n");
+
+        let calculCo2 = 0;
+
+        e.domains.map((domain,index)=>{
+            if(index<KEEP_VALUES)
+             domainString.push(domain.name+" : "+domain.co2+" grammes \n");
+            else
+                calculCo2 += domain.co2;
         })
+
+        if(e.domains.length>KEEP_VALUES)
+            domainString.push("et "+ e.domains.slice(KEEP_VALUES).length +" autres : "+calculCo2+" grammes");
+
+        domainString.push("\n");
+        
         return domainString
     });
 
   const arrayType = ["bar", "doughnut", "pie", "line", "polarArea", "radar"];
   const type = arrayType[Math.floor(Math.random() * arrayType.length)]; // bar doughnut pie line polarArea radar
+  
   const afterBody = (context:any)=>
   {
-    return allDaysdomainsArray[context[0].dataIndex];
+     return allDaysdomainsArray[context[0].dataIndex];
   }
 
   return (
