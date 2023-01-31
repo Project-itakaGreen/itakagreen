0bb00be9f9987f1054c0ea5a44286067ce2db83e
Klod <claude.buisson@evoluflor.fr>
2023-01-31 14:34:37 +0100

    add array comparative sentences for co2 consomption

--- front/pages/search.tsx
+++ front/pages/search.tsx
@@ -9,6 +9,25 @@ export default function Search() {
     { date: "24/01/2023", co2: 6 },
     { date: "25/01/2023", co2: 173 },
   ];
+
+  const compareSentences=[
+    {kilo:0.03, text:"Vous avez consommé l'équivalent d'un trajet de 10km en metro"},
+    {kilo:0.1, text:"Vous avez consommé l'équivalent d'un trajet de 10km en vélo à assistance électrique"},
+    {kilo:0.8, text:"Vous avez consommé l'équivalent d'un trajet de 10km en scooter"},
+    {kilo:1, text:"Vous avez consommé l'équivalent d'un trajet 1km de en voiture électrique"},
+    {kilo:2, text:"Vous avez consommé l'équivalent d'un trajet Paris-Marseille en TGV"},
+    {kilo:2.5, text:"Vous avez consommé l'équivalent d'un trajet de 12km en voiture"},
+    {kilo:3, text:"Vous avez consommé l'équivalent d'un trajet de 102km en autocar ou 0.4 repas avec du boeuf"},
+    {kilo:10, text:"Vous avez consommé l'équivalent d'un trajet de 97km en voiture électrique"},
+    {kilo:27, text:"Vous avez consommé l'équivalent d'un trajet Paris-Marseille en autocar"},
+    {kilo:38, text:"Vous avez consommé l'équivalent d'un an de chauffage au gaz naturel"},
+    {kilo:68, text:"Vous avez consommé l'équivalent d'un trajet Paris-Marseille en co-voiturage"},
+    {kilo:85, text:"Vous avez consommé l'équivalent d'un trajet Paris-Marseille en avion"},
+    {kilo:100, text:"Vous avez consommé l'équivalent de 196 repas végétarien ou 42373km  en TGV"},
+    {kilo:150, text:"Vous avez consommé l'équivalent d'un trajet Paris-Marseille en voiture"},
+    {kilo:200, text:"Vous avez consommé l'équivalent de 392 repas végétarien ou 28 repas avec du boeuf"},
+    {kilo:250, text:"Vous avez consommé l'équivalent d'un trajet de 2 418km en voiture"},
+  ]
   const chartLabel = data.map((e) => e.date);
   const chartData = data.map((e) => e.co2);
   const arrayType = ['bar','doughnut','pie','line','polarArea','radar'];
