77f7653d45be98c4a0a2e2609ae09b78bb567c25
adrien <a.charrier@web-adn.com>
2023-01-31 14:12:46 +0100

    feat-add route to get if the domain run on renewable energie

--- api/src/domain/domain.module.ts
+++ api/src/domain/domain.module.ts
@@ -3,11 +3,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Domain } from '../domain/entities/domain.entity';
+import { DomainController } from './domain.controller';
 import { DomainService } from './domain.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Domain]), HttpModule],
-  controllers: [],
+  imports: [
+    TypeOrmModule.forFeature([Domain]),
+    HttpModule
+  ],
+  controllers: [DomainController],
   providers: [DomainService],
   exports: [DomainService],
 })
