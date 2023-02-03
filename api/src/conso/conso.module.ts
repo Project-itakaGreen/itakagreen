8f7efeeada17a79a440ea478a2862a6a28a7adf6
Klod <claude.buisson@evoluflor.fr>
2023-02-03 11:31:19 +0100

    fix format

--- api/src/conso/conso.module.ts
+++ api/src/conso/conso.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-
 import { ConsoService } from './conso.service';
-
 import { ConsoController } from './conso.controller';
-
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 import { Record } from 'src/record/entities/record.entity';
 
 @Module({
